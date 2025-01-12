bb9b7241ef9a6ded4746d239081d51cc31c7f337
Henrique <henrixe80@gmail.com>
2025-01-12 09:54:33 -0400

    Adicionando as minhas mensagens

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -9,24 +9,29 @@ export class MessageService {
   constructor() { }
   private messages: Message[] = [
     {
-      nome: 'Nazira',
-      mensagem: 'Amo você wgnweingpewngwo´gw~wgemewgm´mweg´mgemweo´mgéwe~~egmg~ewmegmogçwegçeowjwgoweogwegweg~gwemgwemwegmlgm~gemlwm~gmegwem~wegm.',
-      fotos: ['assets/maria.jpg','assets/maria.jpg','assets/maria.jpg','assets/maria.jpg'],
+      nome: 'Henrique',
+      mensagem: 'Primeiramente feliz aniversário querida, construí esse site pra guardar diversas mensagens e momentos tantos meus quanto de quem quiser participar e deixar recadinhos pra você',
+      fotos: ['assets/snop.jpg'],
     },
     {
       nome: 'Henrique',
-      mensagem: 'Amo você .................................................',
-      fotos: ['assets/maria.jpg'],
+      mensagem: 'Sei que tem sido dificil esses tempos, criei esse espaço pensando que poderia te deixar mais animada, ter um ambiente cheio de mensagens de afeto e carinho pra você',
+      fotos: ['assets/snop2.jpg'],
     },
     {
-      nome: 'Kamilly',
-      mensagem: 'Amo você .................................................',
-      fotos: ['assets/maria.jpg'],
+      nome: 'Henrique',
+      mensagem: 'Sou muito feliz por ter você como amiga, me preocupo bastante contigo por isso encho teu saco perguntando se já comeu ou se bebeu água, tu tem mania de esquecer quando fica fazendo as coisas',
+      fotos: ['assets/snop3.jpg'],
     },
     {
-      nome: 'Heliam',
-      mensagem: 'Amo você .................................................',
-      fotos: ['assets/maria.jpg'],
+      nome: 'Henrique',
+      mensagem: 'Já passamos por tanta coisa, agradeço por tudo que fez e faz, tu não tem ideia do quanto já me ajudou. Se eu sou quem sou hoje, com certeza tem influência tua nisso.',
+      fotos: ['assets/snop4.jpg'],
+    },
+    {
+      nome: 'Henrique',
+      mensagem: 'Vou alimentar esse site diariamente e melhorar ele com o tempo. Espero que possa ajudar você a passar pelos momentos dificeis. Nunca se esqueça, você não esta sozinha.',
+      fotos: ['assets/snop5.jpg'],
     },
   ];
 
